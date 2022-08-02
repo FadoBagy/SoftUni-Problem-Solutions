be1f6662bd796d028a6407ef6d094202fc12cc14
Mladen Nedev <mlados43@gmail.com>
2022-08-03 00:08:37 +0300

    EmployeesInformation task

---
+++ C#/Entity Framework Core/Simple Operations/EmployeesInformation/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using var context = new SoftUniContext();
+
+            string result = GetEmployeesFullInformation(context);
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder result = new StringBuilder();
+
+            var allEmployees = context
+                 .Employees
+                 .OrderBy(e => e.EmployeeId)
+                 .Select(e => new
+                 {
+                     e.FirstName,
+                     e.LastName,
+                     e.MiddleName,
+                     e.JobTitle,
+                     e.Salary
+                 })
+                 .ToArray();
+
+            foreach (var e in allEmployees)
+            {
+                result.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}

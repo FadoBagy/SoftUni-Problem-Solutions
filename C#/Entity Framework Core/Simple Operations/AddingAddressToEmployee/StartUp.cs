937ae3bb1825c8d185a677dcb42936c15c11d398
Mladen Nedev <mlados43@gmail.com>
2022-08-03 00:15:34 +0300

    EmployeesDepartment task

---
+++ C#/Entity Framework Core/Simple Operations/AddingAddressToEmployee/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using var context = new SoftUniContext();
+
+            string result = AddNewAddressToEmployee(context);
+            Console.WriteLine(result);
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            StringBuilder result = new StringBuilder();
+
+            var newAddress = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+            context.Addresses.Add(newAddress);
+
+            var nakov = context
+                .Employees
+                .FirstOrDefault(e => e.LastName == "Nakov");
+            nakov.Address = newAddress;
+            context.SaveChanges();
+
+            var addresses = context
+                .Employees
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .Select(e => new
+                {
+                    e.Address.AddressText
+                });
+
+            foreach (var a in addresses)
+            {
+                result.AppendLine(a.AddressText);
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}

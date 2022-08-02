166b3e2e03ac5c32e61f9d799bba4ae3b8d5d5f2
Mladen Nedev <mlados43@gmail.com>
2022-08-03 02:15:25 +0300

    AddressesByTown task

---
+++ C#/Entity Framework Core/Simple Operations/AddressesByTown/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using var context = new SoftUniContext();
+
+            string result = GetAddressesByTown(context);
+            Console.WriteLine(result);
+        }
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            StringBuilder result = new StringBuilder();
+
+            var addresses = context.
+                Addresses
+                .OrderByDescending(a => a.Employees.Count)
+                .ThenBy(a => a.Town.Name)
+                .ThenBy(a => a.AddressText)
+                .Take(10)
+                .Select(a => new
+                {
+                    a.AddressText,
+                    TownName = a.Town.Name,
+                    EmployeesCount = a.Employees.Count
+                })
+                .ToList();
+
+            foreach (var a in addresses)
+            {
+                result.AppendLine($"{a.AddressText}, {a.TownName} - {a.EmployeesCount} employees");
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}

19f11e604654016e13d4a38baa8becf368ad1f6f
Mladen Nedev <mlados43@gmail.com>
2022-08-03 00:13:32 +0300

    EmployeesSalary task

---
+++ C#/Entity Framework Core/Simple Operations/EmployeesSalary/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using var context = new SoftUniContext();
+
+            string result = GetEmployeesWithSalaryOver50000(context);
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder result = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => (e.Salary > 50000))
+                .OrderBy(e => e.FirstName)
+                .ToArray();
+
+            foreach (var e in employees)
+            {
+                result.AppendLine($"{e.FirstName} - {e.Salary:f2}");
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}

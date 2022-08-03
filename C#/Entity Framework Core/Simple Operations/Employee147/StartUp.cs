fc92741e489e3ad3e3df73e79ce9d5f87fcc3ced
Mladen Nedev <mlados43@gmail.com>
2022-08-03 15:42:58 +0300

    Employee147 task

---
+++ C#/Entity Framework Core/Simple Operations/Employee147/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using var context = new SoftUniContext();
+
+            string result = GetEmployee147(context);
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            StringBuilder result = new StringBuilder();
+
+            var employee148 = context
+                .Employees
+                .Where(e => e.EmployeeId == 147)
+                .Select(e => new 
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    EmployeeProjects = e.EmployeesProjects
+                                            .Select(ep => new 
+                                            {
+                                                ProjectName = ep.Project.Name
+                                            })
+                                            .OrderBy(ep => ep.ProjectName)
+                                            .ToArray()
+                })
+                .ToArray();
+
+                foreach (var e in employee148)
+                {
+                    result.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle}");
+                    foreach (var p in e.EmployeeProjects)
+                    {
+                        result.AppendLine(p.ProjectName);
+                    }
+                }
+            
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}

ef0cabe30fec7345e66e24c948bc28824e1275e7
Mladen Nedev <mlados43@gmail.com>
2022-08-03 16:06:14 +0300

    DepartmentsWithMoreThan5Employees task

---
+++ C#/Entity Framework Core/Simple Operations/DepartmentsWithMoreThan5Employees/Models/Town.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace SoftUni.Models
+{
+    public partial class Town
+    {
+        public Town()
+        {
+            Address = new HashSet<Address>();
+        }
+
+        public int TownId { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Address> Address { get; set; }
+    }
+}
---
+++ C#/Entity Framework Core/Simple Operations/DepartmentsWithMoreThan5Employees/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using var context = new SoftUniContext();
+
+            string result = GetDepartmentsWithMoreThan5Employees(context);
+            Console.WriteLine(result);
+        }
+
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            StringBuilder result = new StringBuilder();
+
+            var departments = context
+                .Departments
+                .Where(d => d.Employees.Count > 5)
+                .OrderBy(d => d.Employees.Count)
+                .ThenBy(d => d.Name)
+                .Select(d => new
+                {
+                    DepartmentName = d.Name,
+                    ManagerFirstName = d.Manager.FirstName,
+                    ManagerLastName = d.Manager.LastName,
+                    DepartmentEmployees = d.Employees
+                                            .Select(e => new 
+                                            {
+                                                e.FirstName,
+                                                e.LastName,
+                                                e.JobTitle
+                                            })
+                                            .OrderBy(e => e.FirstName)
+                                            .ThenBy(e => e.LastName)
+                                            .ToArray()
+                })
+                .ToArray();
+
+            foreach (var d in departments)
+            {
+                result.AppendLine($"{d.DepartmentName} – {d.ManagerFirstName} {d.ManagerLastName}");
+                foreach (var e in d.DepartmentEmployees)
+                {
+                    result.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle}");
+                }
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}

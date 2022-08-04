2a80114816399314a331afafe7f86199fbff0555
Mladen Nedev <mlados43@gmail.com>
2022-08-04 22:47:59 +0300

    Code First tasks

---
+++ C#/Entity Framework Core/Code First/FootballBetting/P03_FootballBetting.Data.Common/GlobalConstants.cs
@@ -0,0 +1,30 @@
+﻿namespace P03_FootballBetting.Data.Common
+{
+    public class GlobalConstants
+    {
+        public const int TeamNameMaxLength = 50;
+        public const int TeamLogoUrlMaxLength = 2048;
+        public const int TeamInitialsMaxLength = 4;
+
+        public const int ColorNameMaxLength = 30;
+
+        public const int TownNameMaxLength = 80;
+
+        public const int CountryNameMaxLength = 60;
+
+        public const int PlayerNameMaxLength = 50;
+        public const int PlayerSquadNumberMaxLength = 99;
+
+
+        public const int PositionNameMaxLength = 30;
+
+        public const int GameResultMaxLength = 10;
+
+        public const int BetPredictionMaxLength = 10;
+
+        public const int UsernameMaxLength = 50;
+        public const int PasswordMaxLength = 256;
+        public const int EmailMaxLength = 320;
+        public const int UserNameMaxLength = 747;
+    }
+}
---
+++ C#/Entity Framework Core/Code First/FootballBetting/P03_FootballBetting.Data/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace P03_FootballBetting.Data
+{
+    public class Config
+    {
+        public const string ConnectionString = 
+            @"Server=.;Database=Bet377;Integrated Security=True;";
+    }
+}

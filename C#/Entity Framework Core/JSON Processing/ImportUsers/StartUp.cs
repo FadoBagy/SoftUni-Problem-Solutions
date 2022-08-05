03a114f943bb95af40352c0513065d628ba89b55
Mladen Nedev <mlados43@gmail.com>
2022-08-05 12:09:58 +0300

    ImportUsers task

---
+++ C#/Entity Framework Core/JSON Processing/ImportUsers/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs.User;
+using ProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Mapper.Initialize(cfg => cfg.AddProfile(typeof(ProductShopProfile)));
+            ProductShopContext dbContext = new ProductShopContext();
+            string inputJson = File.ReadAllText(@"..\..\..\Datasets\users.json");
+
+            var result = ImportUsers(dbContext, inputJson);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            ImportUserDto[] userDtos = JsonConvert.DeserializeObject<ImportUserDto[]>(inputJson);
+
+            ICollection<User> users = new List<User>();
+            foreach (ImportUserDto uDto in userDtos)
+            {
+                User user = Mapper.Map<User>(uDto);
+                users.Add(user);
+            }
+
+            context.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+    }
+}

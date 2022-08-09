256ae4cb14fbe1bb2275c0d3306d5e5aee47681e
Mladen Nedev <mlados43@gmail.com>
2022-08-09 19:19:24 +0300

    Adding MusicHub follow up task

---
+++ C#/Entity Framework Core/Code First/MusicHub_AlbumsInfo/StartUp.cs
@@ -0,0 +1,63 @@
+﻿namespace MusicHub
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context =
+                new MusicHubDbContext();
+
+            var resut = ExportAlbumsInfo(context, 9);
+            Console.WriteLine(resut);
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            var sb = new StringBuilder();
+
+            var albums = context.Albums
+                .Where(x => x.ProducerId == producerId)
+                .Select(x => new
+                {
+                    Name = x.Name,
+                    ReleaseDate = x.ReleaseDate.ToString("MM/dd/yyyy"),
+                    ProducerName = x.Producer.Name,
+                    AlbumSongs = x.Songs
+                        .Select(s => new
+                        {
+                            SongName = s.Name,
+                            Price = s.Price,
+                            SongWriterName = s.Writer.Name
+                        }).OrderByDescending(s => s.SongName)
+                        .ThenBy(s => s.SongWriterName)
+                        .ToArray(),
+                    TotalPrice = x.Price
+                })
+                .OrderByDescending(x => x.TotalPrice);
+
+            foreach (var album in albums)
+            {
+                sb.AppendLine($"-AlbumName: {album.Name}");
+                sb.AppendLine($"-ReleaseDate: {album.ReleaseDate}");
+                sb.AppendLine($"-ProducerName: {album.ProducerName}");
+                sb.AppendLine($"-Songs:");
+                int i = 1;
+                foreach (var song in album.AlbumSongs)
+                {
+                    sb.AppendLine($"---#{i++}");
+                    sb.AppendLine($"---SongName: {song.SongName}");
+                    sb.AppendLine($"---Price: {song.Price:F2}");
+                    sb.AppendLine($"---Writer: {song.SongWriterName}");
+                }
+                sb.AppendLine($"-AlbumPrice: {album.TotalPrice:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

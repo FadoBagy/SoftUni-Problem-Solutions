917c31331c83f57fb4c4e000d3ae84f242211658
Mladen Nedev <mlados43@gmail.com>
2022-08-09 19:59:15 +0300

    Adding MusicHub follow up task and renaming folders

---
+++ C#/Entity Framework Core/Code First/MusicHub - SongsAboveDuration/StartUp.cs
@@ -0,0 +1,56 @@
+﻿namespace MusicHub
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context =
+                new MusicHubDbContext();
+
+            var resut = ExportSongsAboveDuration(context, 4);
+            Console.WriteLine(resut);
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            var sb = new StringBuilder();
+
+            var songs = context
+                .Songs
+                .ToArray()
+                .Where(x => x.Duration.TotalSeconds > duration)
+                .Select(x => new
+                {
+                    SongName = x.Name,
+                    PerformerName = x.SongPerformers
+                        .Select(sp => $"{sp.Performer.FirstName} {sp.Performer.LastName}")
+                        .FirstOrDefault(),
+                    WriterName = x.Writer.Name,
+                    AlbumProducer = x.Album.Producer.Name,
+                    Duration = x.Duration.ToString("c")
+                })
+                .OrderBy(x => x.SongName)
+                .ThenBy(x => x.WriterName)
+                .ThenBy(x => x.PerformerName)
+                .ToArray();
+
+            int i = 1;
+            foreach (var song in songs)
+            {
+                sb.AppendLine($"-Song #{i++}");
+                sb.AppendLine($"---SongName: {song.SongName}");
+                sb.AppendLine($"---Writer: {song.WriterName}");
+                sb.AppendLine($"---Performer: {song.PerformerName}");
+                sb.AppendLine($"---AlbumProducer: {song.AlbumProducer}");
+                sb.AppendLine($"---Duration: {song.Duration}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

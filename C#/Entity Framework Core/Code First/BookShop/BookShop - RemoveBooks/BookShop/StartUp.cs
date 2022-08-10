90e38aef8c004187cecc406894e4423e441a8072
Mladen Nedev <mlados43@gmail.com>
2022-08-11 01:31:28 +0300

    adding a BookShop task

---
+++ C#/Entity Framework Core/Code First/BookShop/BookShop - RemoveBooks/BookShop/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace BookShop
+{
+    using BookShop.Initializer;
+    using BookShop.Models.Enums;
+    using Data;
+
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(RemoveBooks(db));
+        }
+
+        public static int RemoveBooks(BookShopContext context) 
+        {
+            var books = context.Books
+                .Where(x => x.Copies < 4200)
+                .ToList();
+
+            context.Books.RemoveRange(books);
+            
+            context.SaveChanges();
+
+            return books.Count;
+        }
+    }
+}

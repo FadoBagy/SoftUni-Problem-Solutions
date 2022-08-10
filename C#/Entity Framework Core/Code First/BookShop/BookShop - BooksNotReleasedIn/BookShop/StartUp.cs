6f5d3d1ec15ae52c4f86004c7bcd713876c44b27
Mladen Nedev <mlados43@gmail.com>
2022-08-10 23:51:57 +0300

    adding a BookShop task

---
+++ C#/Entity Framework Core/Code First/BookShop/BookShop - BooksNotReleasedIn/BookShop/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            int year = int.Parse(Console.ReadLine());
+            var result = GetBooksNotReleasedIn(db, year);
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year) 
+        {
+            var books = context
+                .Books
+                .Where(x => x.ReleaseDate.Value.Year != year)
+                .OrderBy(x => x.BookId)
+                .Select(x => x.Title)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, books);
+        }
+    }
+}

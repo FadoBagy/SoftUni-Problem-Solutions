2a12e2c42eae2619e09bbead14798b8eeb93dbe2
Mladen Nedev <mlados43@gmail.com>
2022-08-10 23:51:14 +0300

    adding a BookShop task

---
+++ C#/Entity Framework Core/Code First/BookShop/BookShop - BooksByCategory/BookShop/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace BookShop
+{
+    using BookShop.Initializer;
+    using BookShop.Models.Enums;
+    using Data;
+
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+            var result = GetBooksByCategory(db, input);
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksByCategory(BookShopContext context, string input) 
+        {
+            var categories = input
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.ToLower())
+                .ToArray();
+
+            var books = context.Books
+                .Where(x => x.BookCategories
+                    .Any(x => categories.Contains(x.Category.Name.ToLower())))
+                .Select(x => x.Title)
+                .OrderBy(Title => Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+    }
+}

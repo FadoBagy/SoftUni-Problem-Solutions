c6a0a13362cfe2d3ab077b7c5df831d5b796fb7f
Mladen Nedev <mlados43@gmail.com>
2022-08-11 01:30:34 +0300

    adding a BookShop task

---
+++ C#/Entity Framework Core/Code First/BookShop/BookShop - CopiesByAuthor/BookShop/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace BookShop
+{
+    using BookShop.Initializer;
+    using BookShop.Models.Enums;
+    using Data;
+
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            var result = CountCopiesByAuthor(db);
+            Console.WriteLine(result);
+
+        }
+
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            var books = context.Authors
+                .Select(x => new
+                {
+                    AuthorName = x.FirstName + " " + x.LastName,
+                    BookCopiesCount =  x.Books.Sum(b => b.Copies)
+                })
+                .OrderByDescending(x => x.BookCopiesCount)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, books.Select(x => $"{x.AuthorName} - {x.BookCopiesCount}"));
+        }
+    }
+}

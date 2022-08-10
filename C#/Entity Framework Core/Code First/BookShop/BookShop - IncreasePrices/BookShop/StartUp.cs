3f54b201234a5dfe7c267b5a1dc1f68cf1dba776
Mladen Nedev <mlados43@gmail.com>
2022-08-11 01:30:56 +0300

    adding a BookShop task

---
+++ C#/Entity Framework Core/Code First/BookShop/BookShop - IncreasePrices/BookShop/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace BookShop
+{
+    using BookShop.Initializer;
+    using BookShop.Models.Enums;
+    using Data;
+
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(x => x.ReleaseDate.Value.Year < 2010);
+
+            foreach (var book in books)
+            {
+                book.Price += 5;
+            }
+            context.SaveChanges();
+        }
+    }
+}
